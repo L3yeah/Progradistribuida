5d4122e6efa90ab93886c0a439d060893c596679
Lilia S. Trujillo Rivera <ltrujillo8@ucol.mx>
2022-05-09 01:09:39 -0500

    Added my project

--- app.js
+++ app.js
@@ -14,29 +14,51 @@ server.listen(puerto, () => {
 
 const express = require("express");
 const app = express();
-const port = 3000;
+//const port = 3000;
 
-app.set("view engine", "ejs");           //agregamos estas dos lineas para el Motor de plantilla
-app.set("views", __dirname + "/views"); 
+//app.set("view engine", "ejs");           //agregamos estas dos lineas para el Motor de plantilla
+//app.set("views", __dirname + "/views"); 
 
-app.use(express.static(__dirname + "/public"));//una pagina estatica que no tiene manejo de datos en ese momento, una pagina que no se mueve, que solo tiene informacion y para eso es el .use para crear unicamente una pagina informativa/ dirname es la raiz de tu proyecto segun donde este
+//app.use(express.static(__dirname + "/public"));//una pagina estatica que no tiene manejo de datos en ese momento, una pagina que no se mueve, que solo tiene informacion y para eso es el .use para crear unicamente una pagina informativa/ dirname es la raiz de tu proyecto segun donde este
 
-app.get("/servicios", (req, res) => {
-  res.render("servicios", { tituloServicios: "Estas en Servicios" });
-});
-
-
-app.get("/", (req, res) => {
-  res.render("index", { titulo: "Inicio EJS en home" });
-});
-
-app.get("/contacto", (req, res) => {
-  res.render("contacto", { tituloContacto: "Estas en Contacto" });
-});
+//app.get("/servicios", (req, res) => {
+ // res.render("servicios", { tituloServicios: "Estas en Servicios" });
+//});
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+//app.get("/", (req, res) => {
+ // res.render("index", { titulo: "Inicio EJS en home" });
+//});
+
+//app.get("/contacto", (req, res) => {
+  //res.render("contacto", { tituloContacto: "Estas en Contacto" });
+//});
+
+
+const http = require('http')
+const server = http.createServer(app)
+const {Server} = require('socket.io')
+const io = new Server(server)
+
+io.on('connection', (socket)=>{
+ // console.log ('Un usuario se ha conectado')
+
+    //socket.on('chat',(msg)=>{
+     // console.log('Mensaje: '+msg)
+   // })
+
+   socket.on('chat', msg =>{
+     io.emit('chat', msg)
+   })
+})
+
+app.get('/', (req, res)=>{
+  //res.send('<h1>Aplicación de CHAT</h1>')
+    res.sendFile(`${__dirname}/chatSockets/index.html`)
+})
+
+server.listen(3000, ()=> {
+  console.log(`Servidor corriendo en http://localhost:${3000}`);
 });
 
 /*
